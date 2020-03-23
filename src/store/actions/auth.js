398d9e6fccc148b7f8818f1f54a4e4167c310050
lunevaes <luneveks@gmail.com>
2020-03-23 23:26:30 +0300

    Добавляет redux для страницы авторизации

---
+++ src/store/actions/auth.js
@@ -0,0 +1,54 @@
+import axios from 'axios'
+import {AUTH_SUCCESS, AUTH_LOGOUT} from './actionTypes'
+
+export function auth (email, password, isLogin) {
+  return async dispatch => {
+    const authData = {
+      email, password,
+      returnSecureToken: true
+    }
+
+    let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCZx7N4U87TE_ThjJyWOn5wrZlONYAAPcw'
+
+    if (isLogin) {
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCZx7N4U87TE_ThjJyWOn5wrZlONYAAPcw'
+    }
+
+    const response = await axios.post(url, authData)
+    const data = response.data
+
+    const expirationDate = new Date(new Date().getTime() + data.expiresIn * 1000)
+
+    localStorage.setItem('token', data.idToken)
+    localStorage.setItem('userId', data.localId)
+    localStorage.setItem('expirationDate', data.expirationDate)
+
+    dispatch(authSuccess(data.idToken))
+    dispatch(autoLogout(data.expiresIn))
+  }
+}
+
+export function autoLogout(time) {
+  return dispatch => {
+    setTimeout(() => {
+      dispatch(logout())
+    }, time * 1000)
+  }
+}
+
+export function logout() {
+  localStorage.removeItem('token')
+  localStorage.removeItem('userId')
+  localStorage.removeItem('expirationDate')
+
+  return {
+    type: AUTH_LOGOUT
+  }
+}
+
+export function authSuccess(token) {
+  return {
+    type: AUTH_SUCCESS,
+    token
+  }
+}
